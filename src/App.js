316cd5b372dde86962d429069cc64ba95121ad8c
lucasGabrielDeAA <lg.technus@gmail.com>
2019-02-16 16:23:44 -0200

    App works

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import { Styles } from './components';
+
+const App = () => (
+  <Styles.Wrapper>
+    <Styles.Title>Hello World, this is my first styled component!</Styles.Title>
+
+    <Styles.Button>Normal Button</Styles.Button>
+    <Styles.TomatoButton>Tomato Button</Styles.TomatoButton>
+  </Styles.Wrapper>
+);
 
 export default App;
---
+++ src/components/Styles/index.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+const Button = styled.button`
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+const TomatoButton = styled(Button)`
+  color: tomato;
+  border-color: tomato;
+`;
+
+const Styles = () => <Fragment />
+
+Styles.Title = Title;
+Styles.Wrapper = Wrapper;
+Styles.Button = Button;
+Styles.TomatoButton = TomatoButton;
+
+export default Styles;
